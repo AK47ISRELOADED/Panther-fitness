8cc80602ff27b734e408c69abbba245bd0dc91cb
adarsh_kaurav <adarshkaurav47@gmail.com>
2024-05-18 14:23:06 +0530

    a fitness app where you can track your fit and diet

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Home = () => {
+  return (
+    <div className="home h-screen w-screen">
+      <Nav />
+    </div>
+  );
+};
+
+export default Home;
